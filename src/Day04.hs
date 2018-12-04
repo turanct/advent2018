56fb8ed7f54b5648dc4e36a921a624d8d059bebe
Toon Daelman <toon.daelman@engagor.com>
2018-12-04 17:41:04 +0100

    ✨ Add way to read day4 input data from string

--- src/Day04.hs
+++ src/Day04.hs
@@ -17,3 +17,21 @@ data Time = Time { y :: Int
 data Log = Log { time :: Time
                , event :: Event
                } deriving (Show, Eq)
+
+-- [1518-11-21 00:01] Guard #641 begins shift
+-- [1518-05-29 00:31] falls asleep
+-- [1518-02-20 00:40] wakes up
+logFromString :: String -> Log
+logFromString s = Log { time = Time { y = y, m = m, d = d, h = h, i = i }, event = event }
+   where y = read $ takeWhile (/= '-') $ drop 1 s
+         m = read $ takeWhile (/= '-') $ dropWhile (== '0') $ drop 1 $ dropWhile (/= '-') s
+         d = read $ takeWhile (/= ' ') $ dropWhile (== '0') $ drop 1 $ dropWhile (/= '-') $ drop 1 $ dropWhile (/= '-') s
+         h = read $ takeWhile (/= ':') $ drop 1 $ dropWhile (/= ' ') s
+         i = read $ takeWhile (/= ']') $ drop 1 $ dropWhile (/= ':') s
+         event = case rest of "falls asleep" -> FellAsleep
+                              "wakes up" -> WokeUp
+                              x -> BeganShift (read $ takeWhile (/= ' ') $ drop 1 $ dropWhile (/= '#') x)
+         rest = drop 2 $ dropWhile (/= ']') s
+
+logsFromStrings :: [String] -> [Log]
+logsFromStrings = map logFromString
--- test/Day04Spec.hs
+++ test/Day04Spec.hs
@@ -7,8 +7,22 @@ import Prelude hiding (id)
 spec :: Spec
 spec = do
   describe "Part 1" $ do
-    it "parses strings to claims" $ do
-      True `shouldBe` True
+    it "parses strings to logs" $ do
+      let strings = [ "[1518-11-21 00:01] Guard #641 begins shift"
+                    , "[1518-05-29 00:31] falls asleep"
+                    , "[1518-02-20 00:40] wakes up"
+                    ]
+      let logs = [ Log { time = Time { y = 1518, m = 11, d = 21, h = 0, i = 1 }
+                       , event = BeganShift 641
+                       }
+                 , Log { time = Time { y = 1518, m = 5, d = 29, h = 0, i = 31 }
+                       , event = FellAsleep
+                       }
+                 , Log { time = Time { y = 1518, m = 2, d = 20, h = 0, i = 40 }
+                       , event = WokeUp
+                       }
+                 ]
+      logsFromStrings strings `shouldBe` logs
 
   -- describe "Part 1 with input from file" $ do
   --   file <- runIO $ readFile "test/day04.txt"
