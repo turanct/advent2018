8793145be80551ba69458447b382c3ea7d643552
turanct <spinnewebber_toon@hotmail.com>
2018-12-04 23:52:02 +0100

    😡 Hated this day... solved it in a really dirty bloated way...

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,5 +1,7 @@
 module Day04 where
 
+import Data.List
+
 type GuardId = Int
 
 data Event = BeganShift GuardId
@@ -18,6 +20,9 @@ data Log = Log { time :: Time
                , event :: Event
                } deriving (Show, Eq)
 
+instance Ord Log where
+  log1 `compare` log2 = time log1 `compare` time log2
+
 -- [1518-11-21 00:01] Guard #641 begins shift
 -- [1518-05-29 00:31] falls asleep
 -- [1518-02-20 00:40] wakes up
@@ -35,3 +40,85 @@ logFromString s = Log { time = Time { y = y, m = m, d = d, h = h, i = i }, event
 
 logsFromStrings :: [String] -> [Log]
 logsFromStrings = map logFromString
+
+data GuardState = Awake GuardId [Time]
+                | Sleeping GuardId Time [Time]
+                deriving (Show, Eq)
+
+changeGuardState :: GuardState -> Log -> GuardState
+changeGuardState (Awake gid mins) Log {time=start, event=FellAsleep} =
+  Sleeping gid start mins
+changeGuardState (Awake gid mins) Log {time=t, event=_} =
+  Awake gid mins
+changeGuardState (Sleeping gid start mins) Log {time=end, event=WokeUp} =
+  Awake gid (minsBetween start end ++ mins)
+changeGuardState (Sleeping gid start mins) Log {time=t, event=_} =
+  Sleeping gid start mins
+
+minsBetween :: Time -> Time -> [Time]
+minsBetween a b = [ Time { y=y a, m=m a, d=d a, h=h a, i=i } | i <- [i a..i b - 1] ]
+
+statesFromLogs :: [Log] -> [GuardState]
+statesFromLogs = reverse . foldl f []
+  where f shifts Log{time=t, event=(BeganShift gid)} = Awake gid [] : shifts
+        f shifts l = changeGuardState (head shifts) l : tail shifts
+
+data Shift = Shift { guard :: GuardId
+                   , minutesAsleep :: [Time]
+                   } deriving (Show, Eq, Ord)
+
+shiftsFromStates :: [GuardState] -> [Shift]
+shiftsFromStates = map f
+  where f (Awake gid mins) = Shift { guard = gid, minutesAsleep = mins }
+        f (Sleeping gid _ mins) = Shift { guard = gid, minutesAsleep = mins }
+
+sameGuard :: Shift -> Shift -> Bool
+sameGuard a b = guard a == guard b
+
+joinShifts :: Shift -> Shift -> Shift
+joinShifts a b = Shift { guard = guard a, minutesAsleep = minutesAsleep a ++ minutesAsleep b }
+
+shiftWithMostSleepingMinutes :: [Shift] -> Shift
+shiftWithMostSleepingMinutes = foldl1 most
+  where most a b = if (length . minutesAsleep) a > (length . minutesAsleep) b then a else b
+
+mostCommonMinute :: Shift -> Int
+mostCommonMinute = head . longestList . group . sort . map i . minutesAsleep
+
+longestList :: [[a]] -> [a]
+longestList [] = []
+longestList [x] = x
+longestList (a:b:cs)
+  | length a > length b = longestList (a:cs)
+  | otherwise = longestList (b:cs)
+
+solvePart1 :: [String] -> Int
+solvePart1 ss = sleepingGuard * minute
+  where sleepingGuard = guard shift
+        minute = mostCommonMinute shift
+        shift = shiftWithMostSleepingMinutes
+              $ map (foldl1 joinShifts)
+              $ groupBy sameGuard
+              $ sort
+              $ shiftsFromStates
+              $ statesFromLogs
+              $ sort
+              $ logsFromStrings ss
+
+shiftWithMostCommonMinutes :: [Shift] -> Shift
+shiftWithMostCommonMinutes = foldl1 most
+  where most a b = if (length . cm) a > (length . cm) b then a else b
+        cm = longestList . group . sort . map i . minutesAsleep
+
+solvePart2 :: [String] -> Int
+solvePart2 ss = sleepingGuard * minute
+  where sleepingGuard = guard shift
+        minute = mostCommonMinute shift
+        shift = shiftWithMostCommonMinutes
+              $ map (foldl1 joinShifts)
+              $ groupBy sameGuard
+              $ sort
+              $ shiftsFromStates
+              $ statesFromLogs
+              $ sort
+              $ logsFromStrings ss
--- test/Day04Spec.hs
+++ test/Day04Spec.hs
@@ -24,9 +24,67 @@ spec = do
                  ]
       logsFromStrings strings `shouldBe` logs
 
-  -- describe "Part 1 with input from file" $ do
-  --   file <- runIO $ readFile "test/day04.txt"
+    it "finds minutes between times" $ do
+      let time1 = Time { y = 1518, m = 1, d = 1, h = 0, i = 2 }
+      let time2 = Time { y = 1518, m = 1, d = 1, h = 0, i = 5 }
+      let expectedMinutes = [ Time { y = 1518, m = 1, d = 1, h = 0, i = 2 }
+                            , Time { y = 1518, m = 1, d = 1, h = 0, i = 3 }
+                            , Time { y = 1518, m = 1, d = 1, h = 0, i = 4 }
+                            ]
+      minsBetween time1 time2 `shouldBe` expectedMinutes
 
-  --   it "solves the puzzle" $ do
-  --     let input = lines file
-  --     overlappingSquares input `shouldBe` 112378
+    it "reads states from logs" $ do
+      let logs = [ Log { time = Time { y = 1518, m = 1, d = 1, h = 0, i = 1 }
+                       , event = BeganShift 1
+                       }
+                 , Log { time = Time { y = 1518, m = 1, d = 1, h = 0, i = 31 }
+                       , event = FellAsleep
+                       }
+                 , Log { time = Time { y = 1518, m = 1, d = 1, h = 0, i = 35 }
+                       , event = WokeUp
+                       }
+                 , Log { time = Time { y = 1518, m = 1, d = 1, h = 0, i = 31 }
+                       , event = BeganShift 2
+                       }
+                 ]
+      let states = [ Awake 1 [ Time { y = 1518, m = 1, d = 1, h = 0, i = 31 }
+                             , Time { y = 1518, m = 1, d = 1, h = 0, i = 32 }
+                             , Time { y = 1518, m = 1, d = 1, h = 0, i = 33 }
+                             , Time { y = 1518, m = 1, d = 1, h = 0, i = 34 }
+                             ]
+                   , Awake 2 []
+                   ]
+      statesFromLogs logs `shouldBe` states
+
+    it "reads shifts from states" $ do
+      let states = [ Awake 1 [ Time { y = 1518, m = 1, d = 1, h = 0, i = 31 }
+                             , Time { y = 1518, m = 1, d = 1, h = 0, i = 32 }
+                             , Time { y = 1518, m = 1, d = 1, h = 0, i = 33 }
+                             , Time { y = 1518, m = 1, d = 1, h = 0, i = 34 }
+                             ]
+                   , Awake 2 []
+                   ]
+      let shifts = [ Shift { guard = 1
+                           , minutesAsleep = [ Time { y = 1518, m = 1, d = 1, h = 0, i = 31 }
+                                             , Time { y = 1518, m = 1, d = 1, h = 0, i = 32 }
+                                             , Time { y = 1518, m = 1, d = 1, h = 0, i = 33 }
+                                             , Time { y = 1518, m = 1, d = 1, h = 0, i = 34 }
+                                             ]
+                           }
+                   , Shift { guard = 2, minutesAsleep = [] }
+                   ]
+      shiftsFromStates states `shouldBe` shifts
+
+  describe "Part 1 with input from file" $ do
+    file <- runIO $ readFile "test/day04.txt"
+
+    it "solves the puzzle" $ do
+      let input = lines file
+      solvePart1 input `shouldBe` 4716
+
+  describe "Part 2 with input from file" $ do
+    file <- runIO $ readFile "test/day04.txt"
+
+    it "solves the puzzle" $ do
+      let input = lines file
+      solvePart2 input `shouldBe` 117061
