f45c4661ef9c89f6f8bdfa9750924c3453c713ca
Toon Daelman <toon.daelman@engagor.com>
2018-12-05 18:16:18 +0100

    🍻 Solved day 5!

--- src/Day05.hs
+++ src/Day05.hs
@@ -16,3 +16,17 @@ collapseAll [] = []
 collapseAll [x] = [x]
 collapseAll s = if length collapsed == length s then s else collapseAll collapsed
   where collapsed = collapsePolymers s
+
+allPossiblePolymers :: String -> [String]
+allPossiblePolymers s = zipWith f letters (repeat s)
+  where letters = [ letter  | letter <- sort $ unique $ map toLower s ]
+        f letter string = filter (\x -> x /= letter && x /= toUpper letter) string
+
+unique :: Eq a => [a] -> [a]
+unique [] = []
+unique [x] = [x]
+unique (x:xs) = x : unique (filter (/= x) xs)
+
+shortestReduction :: [String] -> Int
+shortestReduction = head . sort . lenghts
+  where lenghts = map (length . collapseAll)
--- test/Day05Spec.hs
+++ test/Day05Spec.hs
@@ -18,9 +18,27 @@ spec = do
     it "collapses all Polymers" $ do
       collapseAll "dabAcCaCBAcCcaDA" `shouldBe` "dabCBAcaDA"
 
-  describe "Part 1 with input from file" $ do
-    file <- runIO $ readFile "test/day05.txt"
-    let input = head $ lines file
+  -- describe "Part 1 with input from file" $ do
+  --   file <- runIO $ readFile "test/day05.txt"
+  --   let input = head $ lines file
 
-    it "solves the puzzle" $ do
-      (length . collapseAll) input `shouldBe` 10
+  --   it "solves the puzzle" $ do
+  --     (length . collapseAll) input `shouldBe` 10
+
+  describe "Part 2" $ do
+    it "creates all possible Polymers without one letter" $ do
+      let polymer = "dabAcCaCBAcCcaDA"
+
+      allPossiblePolymers polymer `shouldBe` ["dbcCCBcCcD", "daAcCaCAcCcaDA", "dabAaBAaDA", "abAcCaCBAcCcaA"]
+
+    it "finds the shortest polymer reduction of a list of polymers" $ do
+      let polymers = ["dbcCCBcCcD", "daAcCaCAcCcaDA", "dabAaBAaDA", "abAcCaCBAcCcaA"]
+
+      shortestReduction polymers `shouldBe` 4
+
+  -- describe "Part 2 with input from file" $ do
+  --   file <- runIO $ readFile "test/day05.txt"
+  --   let input = head $ lines file
+
+  --   it "solves the puzzle" $ do
+  --     shortestReduction (allPossiblePolymers input) `shouldBe` 5148
